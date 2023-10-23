3b16756e845ee99a84a496d17ddf9d1cfe0a5552
abdo elsharef <abdoelsharef1@gmail.com>
2023-10-24 01:50:21 +0300

    upload repo

---
+++ CRM/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import *
+
+# registerr your models here.
+admin.site.register(Brand)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display =['name','phone','email']
+    list_filter=['name','phone','email']
+    search_fields=['name',]
+admin.site.register(Company)
+admin.site.register(Issue)
+admin.site.register(Action)
+admin.site.register(Color)
+
+class Follow_UpTabularInline(admin.TabularInline):
+    model = Follow_Up
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [Follow_UpTabularInline,]
+    list_display =['__str__','date','status','barcode','serial','color','brand','client','company','active','purchase_date','issue','received_date','action','delivery_date']
+    list_filter=['id','status','color','brand','client','company','active','purchase_date','issue','received_date','action','delivery_date']
+    search_fields=['barcode','serial',]
+    # exclude = ('id',)
+class Follow_UpAdmin(admin.ModelAdmin):
+    list_display =['__str__','connect','comment']
+    list_filter=['order','connect','comment']
+    search_fields=['comment',]
+admin.site.register(Follow_Up,Follow_UpAdmin)
+
+
